964d2db0db1915eb2a2e5af0ed1077f7028a246a
Francis Ong <francis.ong25@gmail.com>
2022-11-08 23:54:36 +0800

    feat: use shop routes

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 // 3rd Party Modules
 const express = require('express');
-const exp = require('constants');
 
 // Initialization
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+// Routers
+const shopRoutes = require('./routes/shop');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -16,9 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(shopRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);
